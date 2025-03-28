e14f25687453daccff307614e19e11eeee95109f
Admin <smhemalathasm@gmail.com>
2025-03-28 13:15:02 +0530

    10

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ data "aws_vpc" "name" {
 }
 
 data "aws_subnet" "sub" {
-  vpc_id = data.aws_vpc.name
+  vpc_id = data.aws_vpc.name.id
   filter {
     name = "availability-zone"
     values = ["us-east-2a"]
@@ -30,7 +30,7 @@ data "aws_subnet" "sub" {
 }
 
 resource "aws_security_group" "sg-test" {
-  vpc_id = data.aws_vpc.name
+  vpc_id = data.aws_vpc.name.id
   description = "sg for test vpc"
   name = "web-sg-test"
 
@@ -60,11 +60,11 @@ resource "aws_instance" "ec2" {
   ami = "ami-0d8c288225dc75373"
   instance_type = "t2.micro"
   key_name = "ohio"
-  subnet_id = data.aws_subnet.sub
+  subnet_id = data.aws_subnet.sub.id
   vpc_security_group_ids = [aws_security_group.sg-test.id]
   associate_public_ip_address = true
 
-  user_data = file("${path.module}/userdata.sh")
+  #user_data = file("${path.module}/userdata.sh")
 
   tags = {
     name = "test"
